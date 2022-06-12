5b21575a837acec791fb8659ebe996770446c2de
Taha <itaha.kamil@gmail.com>
2022-06-12 20:41:35 +0100

    Setup CorsFilter for frontend requests

--- src/main/java/io/kam/server/ServerApplication.java
+++ src/main/java/io/kam/server/ServerApplication.java
@@ -2,12 +2,16 @@ package io.kam.server;
 
 import io.kam.server.enumeration.Status;
 import io.kam.server.model.Server;
-import io.kam.server.repo.ServerRepo;
 import io.kam.server.service.implementation.ServerServiceImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -24,5 +28,23 @@ public class ServerApplication {
 			service.create(new Server(null, "192.168.1.3", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "", Status.SERVER_UP));
 		};
 	}*/
+
+	@Bean
+	public CorsFilter corsFilter(){
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource =
+				new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method","Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
+
 
 }
